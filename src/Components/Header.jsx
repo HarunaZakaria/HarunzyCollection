d8f617b02609a88411460761bdd013c3fef13a97
Sir Harunzy <Harunzy55@gmail.com>
2025-05-22 15:01:03 +0530

    add a components folder

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import NavBar from "./NavBar"
+export default function Header() {
+    return(
+        <>
+        <NavBar />
+        <h1>My Header Component</h1>
+        <p>This is the header component of the app</p>
+        
+        </>
+    )
+}
