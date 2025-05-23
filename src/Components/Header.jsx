03b26945ee182c938b10282be3dd2515d84dc683
Sir Harunzy <Harunzy55@gmail.com>
2025-05-23 21:56:23 +0530

    add solution

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,8 +7,8 @@ export default function Header() {
             <div className="col-lg-6">
                 <h1>Raining Offers For Hot Summer</h1>
                 <p>25% Off On All Products</p>
-                <button type="button" class="btn btn-lg btn-secondary">Shop Now</button>
-                <button type="button" class="btn btn-lg btn-outline-secondary">Find More</button>
+                <button type="button" className="btn btn-lg btn-secondary">Shop Now</button>
+                <button type="button" className="btn btn-lg btn-outline-secondary">Find More</button>
             </div>
             <div className="col-lg-6">
                 <img src="/images/sandals.jpg" alt="" />
