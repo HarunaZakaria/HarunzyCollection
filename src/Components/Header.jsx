d1d9dd3745318fc736a7bfdc513ab954592455b1
Sir Harunzy <Harunzy55@gmail.com>
2025-05-23 21:47:07 +0530

    render images in the header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     return(
         <div>
         <NavBar />
-        <div className="row">
+        <div className="row header-message">
             <div className="col-lg-6">
                 <h1>Raining Offers For Hot Summer</h1>
                 <p>25% Off On All Products</p>
@@ -11,10 +11,7 @@ export default function Header() {
                 <button type="button" class="btn btn-lg btn-outline-secondary">Find More</button>
             </div>
             <div className="col-lg-6">
-                <h1>Raining Offers For Hot Summer</h1>
-                <p>25% Off On All Products</p>
-                <button type="button" class="btn btn-lg btn-secondary">Shop Now</button>
-                <button type="button" class="btn btn-lg btn-outline-secondary">Find More</button>
+                <img src="/images/sandals.jpg" alt="" />
             </div>
         </div>
         </div>
