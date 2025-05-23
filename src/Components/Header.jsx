cfb9f9b899044bd59907a728b1402ad1b7aac403
Sir Harunzy <Harunzy55@gmail.com>
2025-05-23 21:04:01 +0530

    add content to the header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,22 @@
 import NavBar from "./NavBar"
 export default function Header() {
     return(
-        <>
+        <div>
         <NavBar />
-        <h1>My Header Component</h1>
-        <p>This is the header component of the app</p>
-        
-        </>
+        <div className="row">
+            <div className="col-lg-6">
+                <h1>Raining Offers For Hot Summer</h1>
+                <p>25% Off On All Products</p>
+                <button type="button" class="btn btn-lg btn-secondary">Shop Now</button>
+                <button type="button" class="btn btn-lg btn-outline-secondary">Find More</button>
+            </div>
+            <div className="col-lg-6">
+                <h1>Raining Offers For Hot Summer</h1>
+                <p>25% Off On All Products</p>
+                <button type="button" class="btn btn-lg btn-secondary">Shop Now</button>
+                <button type="button" class="btn btn-lg btn-outline-secondary">Find More</button>
+            </div>
+        </div>
+        </div>
     )
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,40 +1,40 @@
 export default function NavBar() {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Harunzy Collection</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Products</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Categories
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Men</a></li>
-            <li><a class="dropdown-item" href="#">Women</a></li>
-            <li><a class="dropdown-item" href="#">Children</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Blog</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+            <nav className="navbar navbar-expand-lg newNav">
+            <div className="container-fluid">
+                <h1><a className="navbar-brand" href="#">Harunzy Collection</a></h1>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                    <a className="nav-link active newNav" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link newNav" href="#">Products</a>
+                    </li>
+                    <li className="nav-item dropdown newNav">
+                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Categories
+                    </a>
+                    <ul className="dropdown-menu">
+                        <li><a className="dropdown-item" href="#">Men</a></li>
+                        <li><a className="dropdown-item" href="#">Women</a></li>
+                        <li><a className="dropdown-item" href="#">Children</a></li>
+                    </ul>
+                    </li>
+                    <li className="nav-item newNav">
+                    <a className="nav-link" href="#">About</a>
+                    </li>
+                    <li className="nav-item newNav">
+                    <a className="nav-link" href="#">Blog</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+            </nav>
         </div>
     )
 }
