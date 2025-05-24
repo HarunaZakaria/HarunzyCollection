428c34b6464e949e72f0d560aaeef2fa3fbd1ca6
Sir Harunzy <Harunzy55@gmail.com>
2025-05-24 19:57:37 +0530

    add new content in the header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,18 +2,35 @@ import NavBar from "./NavBar"
 export default function Header() {
     return(
         <div>
-        <NavBar />
-        <div className="row header-message">
-            <div className="col-lg-6">
-                <h1>Raining Offers For Hot Summer</h1>
-                <p>25% Off On All Products</p>
-                <button type="button" className="btn btn-lg btn-secondary">Shop Now</button>
-                <button type="button" className="btn btn-lg btn-outline-secondary">Find More</button>
+            <NavBar />
+            {/* Header top section  start*/}
+            <div className="row header-message">
+                <div className="col-lg-6">
+                    <h1>Raining Offers For Hot Summer</h1>
+                    <p>25% Off On All Products</p>
+                    <button type="button" className="btn btn-lg btn-secondary">Shop Now</button>
+                    <button type="button" className="btn btn-lg btn-outline-secondary">Find More</button>
+                </div>
+                <div className="col-lg-6">
+                    <img src="/images/sandals.jpg" alt="" />
+                </div>
             </div>
-            <div className="col-lg-6">
-                <img src="/images/sandals.jpg" alt="" />
+            {/* Header top section  end*/}
+
+            {/* Categories section start */}
+
+            <div className="row header-message">
+                <div className="col-lg-4">
+                    <img src="/images/sandals.jpg" alt="picture" />
+                </div>
+                <div className="col-lg-4">
+                    <img src="/images/sandals.jpg" alt="picture" />
+                </div>
+                <div className="col-lg-4">
+                    <img src="/images/sandals.jpg" alt="picture" />
+                </div>
             </div>
-        </div>
+            {/* Categories section end */}
         </div>
     )
 }
