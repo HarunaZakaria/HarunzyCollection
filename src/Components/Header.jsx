232a85282efa65f8be4969c06c424e7e4341d5f7
Sir Harunzy <Harunzy55@gmail.com>
2025-05-24 20:59:16 +0530

    add a card with items

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -20,9 +20,10 @@ export default function Header() {
             {/* Categories section start */}
 
             <div className="row header-message">
+                <h2>Categories</h2>
                 <div className="col-lg-4">
                     <img src="/images/sandals.jpg" alt="picture" />
-                    <h2>20% Off On Tank Tops</h2>
+                    <h3>20% Off On Tank Tops</h3>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
                         Explicabo eveniet tenetur adipisci debitis voluptas accusamus 
                         voluptatum pariatur voluptate nostrum ut.
@@ -31,7 +32,7 @@ export default function Header() {
                 </div>
                 <div className="col-lg-4">
                     <img src="/images/sandals.jpg" alt="picture" />
-                    <h2>20% Off On Tank Tops</h2>
+                    <h3>20% Off On Tank Tops</h3>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
                         Explicabo eveniet tenetur adipisci debitis voluptas accusamus 
                         voluptatum pariatur voluptate nostrum ut.
@@ -40,15 +41,62 @@ export default function Header() {
                 </div>
                 <div className="col-lg-4">
                     <img src="/images/sandals.jpg" alt="picture" />
-                    <h2>20% Off On Tank Tops</h2>
+                    <h3>20% Off On Tank Tops</h3>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
                         Explicabo eveniet tenetur adipisci debitis voluptas accusamus 
                         voluptatum pariatur voluptate nostrum ut.
                     </p>
-                    <button type="button" className="btn btn-warning">Shop Now</button>
+                    <button type="button" className="btn btn-warning">Check Out</button>
                 </div>
             </div>
             {/* Categories section end */}
+
+            {/* Feature products section end */}
+            <div className="row">
+                <h2>Feature Products</h2>
+                <div className="col-lg-3">
+                    <div className="card">
+                        <img src="/images/sandals.jpg" alt="picture" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                 <div className="col-lg-3">
+                    <div className="card">
+                        <img src="/images/sandals.jpg" alt="picture" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                 <div className="col-lg-3">
+                    <div className="card">
+                        <img src="/images/sandals.jpg" alt="picture" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                 <div className="col-lg-3">
+                    <div className="card">
+                        <img src="/images/sandals.jpg" alt="picture" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            {/* Feature products section end */}
         </div>
     )
 }
