80855815dcf89dc9a2a271a72765b8b454ee34d3
Sir Harunzy <Harunzy55@gmail.com>
2025-05-25 18:25:28 +0530

    rename some classes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -110,25 +110,25 @@ export default function Header() {
                     <button type="button" className="btn btn-lg btn-outline-primary">Shop Now</button>
                 </div>
                 <div className="col-lg-6">
-                    <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                        <img src="/images/sandals.jpg" class="d-block w-50" alt="..."/>
+                    <div id="carouselExample" className="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                        <img src="/images/sandals.jpg" className="d-block w-50" alt="..."/>
                         </div>
-                        <div class="carousel-item">
-                        <img src="/images/sandals.jpg" class="d-block w-50" alt="..."/>
+                        <div className="carousel-item">
+                        <img src="/images/sandals.jpg" className="d-block w-50" alt="..."/>
                         </div>
-                        <div class="carousel-item">
-                        <img src="/images/sandals.jpg" class="d-block w-50" alt="..."/>
+                        <div className="carousel-item">
+                        <img src="/images/sandals.jpg" className="d-block w-50" alt="..."/>
                         </div>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
                     </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
                     </button>
                     </div>
                 </div>
