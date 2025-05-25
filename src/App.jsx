4b7a72a626fe99bcf7cc95f7407d6a97a63d7938
Sir Harunzy <Harunzy55@gmail.com>
2025-05-26 03:30:37 +0530

    add a react routers link

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Contact from './Components/Contact';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header />
-      <Footer />      
+      
+      <Router>
+          <Header />
+          <Footer /> 
+          <Routes>
+            {/* <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} /> */}
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>     
     </>
-  )
+  );
 }
 
 export default App
