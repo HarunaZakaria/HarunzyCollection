600d023b547f96e42302864e880a26cefc653855
Sir Harunzy <Harunzy55@gmail.com>
2025-05-25 20:50:12 +0530

    add content to footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,6 +2,39 @@ export default function Footer(){
     return (
         <div>
             <h2>Sales To 70% Off All Sandals On All Styles</h2>
+            <hr />
+            <div className="row">
+                <div className="col-lg-3">
+                    <h1>Harunzy Collection</h1>
+                    <p>The best wear anytime, anywhere.</p>
+                </div>
+                <div className="col-lg-3">
+                    <h1>Men</h1>
+                    <ul>
+                        <li>Simple Wear</li>
+                        <li>Cross Wear</li>
+                        <li>Two belt Wear</li>
+                        <li>Three Wear</li>
+                    </ul>
+                </div>
+                <div className="col-lg-3">
+                    <h1>Women</h1>
+                    <ul>
+                        <li>Simple Wear</li>
+                        <li>Cross Wear</li>
+                        <li>Two belt Wear</li>
+                        <li>Three Wear</li>
+                    </ul>
+                </div>
+                 <div className="col-lg-3">
+                    <h1>Subscribe</h1>
+                    <form action="#">
+                        <input type="email"  placeholder="Your email address"/>
+                        
+                    </form>
+                    <button className="btn btn-lg btn-primary" type="button">SUBSCRIBE</button>
+                </div>
+            </div>
         </div>
     )
 }
