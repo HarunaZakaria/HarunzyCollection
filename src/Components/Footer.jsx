f03ca2ad3dc804b5ff2c1725588cc4cf52399827
Sir Harunzy <Harunzy55@gmail.com>
2025-05-26 01:36:05 +0530

    add complete footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,47 +2,46 @@ export default function Footer(){
     return (
         <div>
             <h2>Sales To 70% Off All Sandals On All Styles</h2>
-            <hr />
             <div className="row">
-                <div className="col-lg-3">
-                    <h1>Harunzy Collection</h1>
-                    <p>The best wear anytime, anywhere.</p>
+                <div className="container">
+                    <footer class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+                        <div class="col mb-3"> 
+                            <p class="col-md-4 mb-0 text-body-secondary">&copy; 2025  Harunzy Collection</p> 
+                        </div> 
+                        <div class="col mb-3"> <h5>Section</h5> 
+                            <ul class="nav flex-column"> 
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li> 
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li> 
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li> 
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li> 
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li> 
+                            </ul> 
+                        </div> 
+                        <div class="col mb-3"> 
+                            <h5>Section</h5> 
+                            <ul class="nav flex-column"> 
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li> 
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li> 
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li> 
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li> 
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li> 
+                            </ul> 
+                        </div> 
+                        <div class="col mb-3"> 
+                            <form> 
+                                <h5>Subscribe</h5> 
+                                <p>Monthly digest of what's new and exciting from us.</p> 
+                                <div class="d-flex flex-column flex-sm-row w-100 gap-2"> 
+                                    <label for="newsletter1" class="visually-hidden">Email address</label> 
+                                    <input id="newsletter1" type="email" class="form-control" placeholder="Email address" />
+                                    <button class="btn btn-primary" type="button">SUBSCRIBE</button> 
+                                </div> 
+                            </form> 
+                        </div>
+                       
+                    </footer>
                 </div>
-                <div className="col-lg-3">
-                    <h1>Men</h1>
-                    <ul>
-                        <li>Simple Wear</li>
-                        <li>Cross Wear</li>
-                        <li>Two belt Wear</li>
-                        <li>Three Wear</li>
-                    </ul>
-                </div>
-                <div className="col-lg-3">
-                    <h1>Women</h1>
-                    <ul>
-                        <li>Simple Wear</li>
-                        <li>Cross Wear</li>
-                        <li>Two belt Wear</li>
-                        <li>Three Wear</li>
-                    </ul>
-                </div>
-                 <div className="col-lg-3">
-                    <h1>Subscribe</h1>
-                    <form action="#">
-                        <input type="email"  placeholder="Your email address"/>
-                        
-                    </form>
-                    <button className="btn btn-lg btn-primary" type="button">SUBSCRIBE</button>
-                </div>
-            </div>
-            <hr />
-            <div className="row">
-                <div className="col-lg-6">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus vitae facilis, tenetur at odit ullam itaque labore ratione iusto cupiditate.</p>
-                </div>
-                <div className="col-lg-6">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, eaque. Expedita iusto eaque corrupti unde obcaecati, voluptas quod incidunt provident!</p>
-                </div>
+                
             </div>
         </div>
     )
