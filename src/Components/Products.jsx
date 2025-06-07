8d4aec49003c4b2261d4e334a28a5fa4d9c2b984
Sir Harunzy <Harunzy55@gmail.com>
2025-06-07 12:10:08 +0000

    add

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,169 +1,234 @@
-export default function Products(){
-    return(
-        <div className="produccts">
-            {/* Categories section start */}
-            <div className="row  Categories">
-                <h2>Categories</h2>
-                <div className="col-lg-4 cate-back1 shadow-lg">
-                    {/* <img src="/images/sandals.jpg" alt="picture" /> */}
-                    <h3>20% Off On Tank Tops</h3>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                        Explicabo eveniet tenetur adipisci debitis voluptas accusamus 
-                        voluptatum pariatur voluptate nostrum ut.
-                    </p>
-                </div>
-                <div className="col-lg-4 cate-back2 shadow-lg">
-                    {/* <img src="/images/sandals.jpg" alt="picture" /> */}
-                    <h3>20% Off On Tank Tops</h3>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                        Explicabo eveniet tenetur adipisci debitis voluptas accusamus 
-                        voluptatum pariatur voluptate nostrum ut.
-                    </p>
-                </div>
-                <div className="col-lg-4 cate-back3 shadow-lg">
-                    {/* <img src="/images/sandals.jpg" alt="picture" /> */}
-                    <h3>20% Off On Tank Tops</h3>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                        Explicabo eveniet tenetur adipisci debitis voluptas accusamus 
-                        voluptatum pariatur voluptate nostrum ut.
-                    </p>
-                </div>
-            </div>
-            {/* Categories section end */}
-
-            {/* Feature products section end */}
-            <div className="row feature-products">
-                <h2>Feature Products</h2>
-                <div className="col-lg-3">
-                    <div className="card">
-                        <img src="/images/sandals0.jpg" alt="picture" />
-                        <div className="card-body">
-                            <h5 className="card-title">BirkenStock Sandals</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                            <p><span className="cross-price">Ghc150 </span> Ghc130</p>
-                            <p className="stars">Rate <span>
-                                <i className="fa fa-star checked"></i>
-                                <i className="fa fa-star checked"></i>
-                                <i className="fa fa-star checked"></i>
-                                <i className="fa fa-star checked"></i>
-                                 <i className="fa-solid fa-star-half-stroke"></i>
-                                </span>
-                            </p>
-                            <a href="#" className="btn btn-primary">Add To Cart</a>
-                        </div>
-                    </div>
-                </div>
-                 <div className="col-lg-3">
-                    <div className="card">
-                        <img src="/images/HarunzyCollection0.jpg" alt="picture" />
-                        <div className="card-body">
-                            <h5 className="card-title">BirkenStock Sandals</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                            <p><span className="cross-price">Ghc200 </span> Ghc180</p>
-                            <p>Rate <span>
-                                <i className="fa fa-star checked stars"></i>
-                                <i className="fa fa-star checked stars"></i>
-                                <i className="fa fa-star checked stars"></i>
-                                <i className="fa fa-star checked stars"></i>
-                                 <i className="fa-solid fa-star-half-stroke stars"></i>
-                                </span>
-                            </p>
-                            <a href="#" className="btn btn-primary">Add To Cart</a>
-                        </div>
-                    </div>
-                </div>
-                 <div className="col-lg-3">
-                    <div className="card">
-                        <img src="/images/sandals2.jpg" alt="picture" />
-                        <div className="card-body">
-                            <h5 className="card-title">BirkenStock Sandals</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                            <p><span className="cross-price">Ghc150 </span> Ghc130</p>
-                            <p className="stars">Rate <span>
-                                <i className="fa fa-star checked"></i>
-                                <i className="fa fa-star checked"></i>
-                                <i className="fa fa-star checked"></i>
-                                <i className="fa fa-star checked"></i>
-                                 <i className="fa-solid fa-star-half-stroke"></i>
-                                </span>
-                            </p>
-                            <a href="#" className="btn btn-primary">Add To Cart</a>
-                        </div>
-                    </div>
-                </div>
-                 <div className="col-lg-3">
-                    <div className="card small">
-                        <img src="/images/sandals1.jpg" alt="picture" />
-                        <div className="card-body">
-                            <h5 className="card-title">BirkenStock Sandals</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                            <p><span className="cross-price">Ghc200 </span> Ghc180</p>
-                            <p className="stars">Rate <span>
-                                <i className="fa fa-star checked"></i>
-                                <i className="fa fa-star checked"></i>
-                                <i className="fa fa-star checked"></i>
-                                <i className="fa fa-star checked"></i>
-                                <i className="fa-solid fa-star-half-stroke"></i>
-                                </span>
-                            </p>
-                            <a href="#" className="btn btn-primary">Add To Cart</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/* Feature products section end */}
-
-            {/* Special offers section end */}
-            <div className="row special-offers">
-                <h3>Limited Offers</h3>
-                <div className="col-lg-6 special-message">
-                    <h1>Special Edition</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                        Eum aspernatur accusamus labore similique sequi modi. 
-                        Pariatur cupiditate exercitationem aperiam minima.
-                    </p>
-                    <h2>Buy This Sandals At 20% Discount, Use Code OFF20</h2>
-                    <button type="button" className="btn btn-lg btn-outline-primary">Shop Now</button>
-                </div>
-                <div className="col-lg-6">
-                    <div id="carouselExample" className="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
-                    <div className="carousel-inner">
-                        <div className="carousel-item active ">
-                        <img src="/images/sandals.jpg" className="d-block w-50 special-sandals" alt="sandals"/>
-                        </div>
-                        <div className="carousel-item">
-                        <img src="/images/sandals.jpg" className="d-block w-50 special-sandals" alt="sandals"/>
-                        </div>
-                        <div className="carousel-item">
-                        <img src="/images/sandals.jpg" className="d-block w-50 special-sandals" alt="sandals"/>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            </div>
-            {/* Special offers section end */}
-
-            {/* Values Section  Start*/}
-            <div className="row values">
-                <h2>Key Values</h2>
-                <div className="col-lg-3 values-message">
-                    <h3>Nationwide Delivery</h3>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero, aut?</p>
-                </div>
-                 <div className="col-lg-3 values-message">
-                    <h3>Best Quality</h3>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero, aut?</p>
-                </div>
-                 <div className="col-lg-3 values-message">
-                    <h3>Best Offers</h3>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero, aut?</p>
-                </div>
-                 <div className="col-lg-3 values-message">
-                    <h3>Easy payment</h3>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero, aut?</p>
-                </div>
-            </div>
-            {/* Values Section end */}
+export default function Products() {
+  return (
+    <div className="produccts">
+      {/* Categories section start */}
+      <div className="row  Categories">
+        <h2>Categories</h2>
+        <div className="col-lg-4 cate-back1 shadow-lg">
+          {/* <img src="/images/sandals.jpg" alt="picture" /> */}
+          <h3>Men Sandals</h3>
+          <p>We sell all kinds of Men Birkenstock and an affordable prices</p>
         </div>
-    );
+        <div className="col-lg-4 cate-back2 shadow-lg">
+          {/* <img src="/images/sandals.jpg" alt="picture" /> */}
+          <h3>Men Crosses</h3>
+          <p>
+            We sell kinds Birkenstock crosses for both men and women at an
+            affordable prices
+          </p>
+        </div>
+        <div className="col-lg-4 cate-back3 shadow-lg">
+          {/* <img src="/images/sandals.jpg" alt="picture" /> */}
+          <h3>Women Sandals</h3>
+          <p>
+            We sell all kinds of Birkenstock sandals for ladies at an affordable
+            prices
+          </p>
+        </div>
+      </div>
+      {/* Categories section end */}
+
+      {/* Feature products section end */}
+      <div className="row feature-products">
+        <h2>Feature Products</h2>
+        <div className="col-lg-3">
+          <div className="card">
+            <img src="/images/sandals0.jpg" alt="picture" />
+            <div className="card-body">
+              <h5 className="card-title">BirkenStock Sandals</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card’s content.
+              </p>
+              <p>
+                <span className="cross-price">Ghc150 </span> Ghc130
+              </p>
+              <p className="stars">
+                Rate{' '}
+                <span>
+                  <i className="fa fa-star checked"></i>
+                  <i className="fa fa-star checked"></i>
+                  <i className="fa fa-star checked"></i>
+                  <i className="fa fa-star checked"></i>
+                  <i className="fa-solid fa-star-half-stroke"></i>
+                </span>
+              </p>
+              <a href="#" className="btn btn-primary">
+                Add To Cart
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3">
+          <div className="card">
+            <img src="/images/HarunzyCollection0.jpg" alt="picture" />
+            <div className="card-body">
+              <h5 className="card-title">BirkenStock Sandals</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card’s content.
+              </p>
+              <p>
+                <span className="cross-price">Ghc200 </span> Ghc180
+              </p>
+              <p>
+                Rate{' '}
+                <span>
+                  <i className="fa fa-star checked stars"></i>
+                  <i className="fa fa-star checked stars"></i>
+                  <i className="fa fa-star checked stars"></i>
+                  <i className="fa fa-star checked stars"></i>
+                  <i className="fa-solid fa-star-half-stroke stars"></i>
+                </span>
+              </p>
+              <a href="#" className="btn btn-primary">
+                Add To Cart
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3">
+          <div className="card">
+            <img src="/images/sandals2.jpg" alt="picture" />
+            <div className="card-body">
+              <h5 className="card-title">BirkenStock Sandals</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card’s content.
+              </p>
+              <p>
+                <span className="cross-price">Ghc150 </span> Ghc130
+              </p>
+              <p className="stars">
+                Rate{' '}
+                <span>
+                  <i className="fa fa-star checked"></i>
+                  <i className="fa fa-star checked"></i>
+                  <i className="fa fa-star checked"></i>
+                  <i className="fa fa-star checked"></i>
+                  <i className="fa-solid fa-star-half-stroke"></i>
+                </span>
+              </p>
+              <a href="#" className="btn btn-primary">
+                Add To Cart
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3">
+          <div className="card small">
+            <img src="/images/sandals1.jpg" alt="picture" />
+            <div className="card-body">
+              <h5 className="card-title">BirkenStock Sandals</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card’s content.
+              </p>
+              <p>
+                <span className="cross-price">Ghc200 </span> Ghc180
+              </p>
+              <p className="stars">
+                Rate{' '}
+                <span>
+                  <i className="fa fa-star checked"></i>
+                  <i className="fa fa-star checked"></i>
+                  <i className="fa fa-star checked"></i>
+                  <i className="fa fa-star checked"></i>
+                  <i className="fa-solid fa-star-half-stroke"></i>
+                </span>
+              </p>
+              <a href="#" className="btn btn-primary">
+                Add To Cart
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Feature products section end */}
+
+      {/* Special offers section end */}
+      <div className="row special-offers">
+        <h3>Limited Offers</h3>
+        <div className="col-lg-6 special-message">
+          <h1>Special Edition</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
+            aspernatur accusamus labore similique sequi modi. Pariatur
+            cupiditate exercitationem aperiam minima.
+          </p>
+          <h2>Buy This Sandals At 20% Discount, Use Code OFF20</h2>
+          <button type="button" className="btn btn-lg btn-outline-primary">
+            Shop Now
+          </button>
+        </div>
+        <div className="col-lg-6">
+          <div
+            id="carouselExample"
+            className="carousel slide"
+            data-bs-ride="carousel"
+            data-bs-interval="5000"
+          >
+            <div className="carousel-inner">
+              <div className="carousel-item active ">
+                <img
+                  src="/images/sandals.jpg"
+                  className="d-block w-50 special-sandals"
+                  alt="sandals"
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  src="/images/sandals.jpg"
+                  className="d-block w-50 special-sandals"
+                  alt="sandals"
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  src="/images/sandals.jpg"
+                  className="d-block w-50 special-sandals"
+                  alt="sandals"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Special offers section end */}
+
+      {/* Values Section  Start*/}
+      <div className="row values">
+        <h2>Key Values</h2>
+        <div className="col-lg-3 values-message">
+          <h3>Nationwide Delivery</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero,
+            aut?
+          </p>
+        </div>
+        <div className="col-lg-3 values-message">
+          <h3>Best Quality</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero,
+            aut?
+          </p>
+        </div>
+        <div className="col-lg-3 values-message">
+          <h3>Best Offers</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero,
+            aut?
+          </p>
+        </div>
+        <div className="col-lg-3 values-message">
+          <h3>Easy payment</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero,
+            aut?
+          </p>
+        </div>
+      </div>
+      {/* Values Section end */}
+    </div>
+  );
 }
