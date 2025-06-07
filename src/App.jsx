9079ea04a0f4b8e4b9dee31292e92917fde49dc9
Sir Harunzy <Harunzy55@gmail.com>
2025-06-07 10:37:07 +0000

    add

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,23 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Contact from './Components/Contact';
 import Products from './Components/Products';
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
-      
       <Router>
-          <Header />
-          <Products />
-          <Footer /> 
-          <Routes>
-            {/* <Route path="/" element={<Home />} />
+        <Header />
+        <Products />
+        <Footer />
+        <Routes>
+          {/* <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} /> */}
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </Router>     
+          {/* <Route path="/contact" element={<Contact />} /> */}
+        </Routes>
+      </Router>
     </>
   );
 }
 
-export default App
+export default App;
