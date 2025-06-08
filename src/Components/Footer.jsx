d980f6e3bf90ff4f080a9d6dfda3aaba1bcbfc9f
Sir Harunzy <Harunzy55@gmail.com>
2025-06-08 21:28:22 +0000

    new commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,50 +1,103 @@
-export default function Footer(){
-    return (
-        <div>
-            
-            <div className="row footer">
-                <h2>Sales To 70% Off All Sandals On All Styles</h2>
-                <div className="container">
-                    <footer className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-                        <div className="col mb-3  footer-content logo"> 
-                            <h5>Harunzy Collection</h5>
-                            <p>The best look anytime, anywhere.</p>
-                        </div> 
-                        <div className="col mb-3 footer-content"> 
-                            <h5>Section</h5> 
-                            <ul className="nav flex-column"> 
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li> 
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li> 
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li> 
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li> 
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li> 
-                            </ul> 
-                        </div> 
-                        <div className="col mb-3 footer-content"> 
-                            <h5>Section</h5> 
-                            <ul className="nav flex-column"> 
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li> 
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li> 
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li> 
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li> 
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li> 
-                            </ul> 
-                        </div> 
-                        <div className="col mb-3 footer-content"> 
-                            <form> 
-                                <h5>Subscribe</h5> 
-                                <p>Monthly digest of what's new and exciting from us.</p> 
-                                <div className="d-flex flex-column flex-sm-row w-100 gap-2"> 
-                                    <label className="visually-hidden">Email address</label> 
-                                    <input id="newsletter1" type="email" className="form-control" placeholder="Email address" />
-                                    <button className="btn btn-primary" type="button">SUBSCRIBE</button> 
-                                </div> 
-                            </form> 
-                        </div>  
-                    </footer>
-                    <p className="col-md-4 copy ">&copy; 2025  Harunzy Collection</p>
-                </div>  
+export default function Footer() {
+  return (
+    <div>
+      <div className="row footer">
+        <h2>Sales To 10% Off All Sandals On All Styles</h2>
+        <div className="container">
+          <footer className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+            <div className="col mb-3  footer-content logo">
+              <h5>SHOP</h5>
+              <h6>Harunzy Collection</h6>
+              <p>The best look anytime, anywhere.</p>
             </div>
+            <div className="col mb-3 footer-content">
+              <h5>POPULAR</h5>
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2">
+                  <a
+                    href="#"
+                    className="nav-link p-0 text-body-secondary footer-link"
+                  >
+                    Seasonal sandals
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a
+                    href="#"
+                    className="nav-link p-0 text-body-secondary footer-link"
+                  >
+                    Must have sandals
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a
+                    href="#"
+                    className="nav-link p-0 text-body-secondary footer-link"
+                  >
+                    Trendy sandals
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col mb-3 footer-content">
+              <h5>SUPPORT</h5>
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link p-0 text-body-secondary">
+                    Contact Us
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link p-0 text-body-secondary">
+                    Features
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link p-0 text-body-secondary">
+                    Account
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link p-0 text-body-secondary">
+                    Store location
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link p-0 text-body-secondary">
+                    Delivery
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col mb-3 footer-content">
+              <h5>INFO</h5>
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link p-0 text-body-secondary">
+                    About Us
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link p-0 text-body-secondary">
+                    Prices
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link p-0 text-body-secondary">
+                    Collections
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link p-0 text-body-secondary">
+                    New sandals
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </footer>
+          <p className="col-md-4 copy ">&copy; 2025 Harunzy Collection</p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
