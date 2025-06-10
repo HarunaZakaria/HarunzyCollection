b3c9dbbbf5858d3e109dd34990e6ab2ebbb93131
Sir Harunzy <Harunzy55@gmail.com>
2025-06-10 03:11:40 +0000

    add testimonials

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -3,38 +3,6 @@ export default function Testimonial() {
     <>
       <div className="testimonials">
         <h2>What Our Customers Say About Us</h2>
-        {/* <div
-          id="testimonial-carousel"
-          class="carousel slide"
-          data-bs-ride="false"
-        >
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <h2>
-                I no longer have to sniff other dogs for love. I've found the
-                hottest Corgi on TinDog. Woof.
-              </h2>
-              <img
-                class="testi-image"
-                src="images/HC15.jpg"
-                alt="dog-profile"
-              />
-              <em>Pebbles, New York</em>
-            </div>
-            <div class="carousel-item">
-              <h2 class="testimonial-text">
-                My dog used to be so lonely, but with TinDog's help, they've
-                found the love of their life. I think.
-              </h2>
-              <img
-                class="testi-image"
-                src="images/HC8.jpg"
-                alt="lady-profile"
-              />
-              <em>Beverly, Illinois</em>
-            </div>
-          </div>
-        </div> */}
         <div className="row">
           <div className="col">
             <div
@@ -46,8 +14,10 @@ export default function Testimonial() {
               <div className="carousel-inner">
                 <div className="carousel-item active ">
                   <p class="testimonial-text">
-                    My sandals was exactly the way I wanted it, thank you so
-                    much for that!.
+                    Harunzy Collection made my day! The Birkenstocks I ordered
+                    fit perfectly and feel like walking on clouds. I'm so happy
+                    with the quality and the excellent customer service. Thank
+                    you!
                   </p>
                   <img
                     class="testi-image"
@@ -58,8 +28,10 @@ export default function Testimonial() {
                 </div>
                 <div className="carousel-item">
                   <p class="testimonial-text">
-                    My sandals was exactly the way I wanted it, thank you so
-                    much for that!.
+                    Five stars for Harunzy Collection! These Birkenstocks are a
+                    game-changer. My daily walks are now a joy thanks to the
+                    incredible support and comfort. So glad I found your
+                    business!
                   </p>
                   <img
                     class="testi-image"
@@ -70,8 +42,10 @@ export default function Testimonial() {
                 </div>
                 <div className="carousel-item">
                   <p class="testimonial-text">
-                    My sandals was exactly the way I wanted it, thank you so
-                    much for that!
+                    I can't express how much I love my Birkenstocks from Harunzy
+                    Collection. They are incredibly comfortable and stylish
+                    exactly what I needed. So thankful for your amazing
+                    selection and service!
                   </p>
                   <img
                     class="testi-image"
@@ -82,8 +56,10 @@ export default function Testimonial() {
                 </div>
                 <div className="carousel-item">
                   <p class="testimonial-text">
-                    My sandals was exactly the way I wanted it, thank you so
-                    much for that!.
+                    A huge thank you to Harunzy Collection! I've been wanting a
+                    pair of Birkenstocks for ages, and these are everything I
+                    hoped for and more. My feet have never been happier.
+                    Fantastic quality and a seamless shopping experience.
                   </p>
                   <img
                     class="testi-image"
@@ -94,8 +70,81 @@ export default function Testimonial() {
                 </div>
                 <div className="carousel-item">
                   <p class="testimonial-text">
-                    My sandals was exactly the way I wanted it, thank you so
-                    much for that!.
+                    Harunzy Collection, you've completely won me over! My new
+                    Birkenstocks are perfect. The comfort is unmatched, and I'm
+                    so grateful for how quickly they arrived. Truly happy with
+                    my purchase!
+                  </p>
+                  <img
+                    class="testi-image"
+                    src="images/HC4.jpg"
+                    alt="lady-profile"
+                  />
+                  <em>Beverly, Illinois</em>
+                </div>
+                <div className="carousel-item">
+                  <p class="testimonial-text">
+                    Honestly, I'm beyond impressed with Harunzy Collection. My
+                    new sandals are not only fashionable but also provide the
+                    best comfort I've ever experienced. So grateful for such a
+                    wonderful product!
+                  </p>
+                  <img
+                    class="testi-image"
+                    src="images/HC4.jpg"
+                    alt="lady-profile"
+                  />
+                  <em>Beverly, Illinois</em>
+                </div>
+                <div className="carousel-item">
+                  <p class="testimonial-text">
+                    What a find Harunzy Collection is! My Birkenstocks arrived
+                    beautifully packaged, and they are even better in person.
+                    I'm incredibly grateful for the high-quality footwear and
+                    the overall pleasant experience.
+                  </p>
+                  <img
+                    class="testi-image"
+                    src="images/HC4.jpg"
+                    alt="lady-profile"
+                  />
+                  <em>Beverly, Illinois</em>
+                </div>
+                <div className="carousel-item">
+                  <p class="testimonial-text">
+                    My feet are singing praises to Harunzy Collection! These
+                    Birkenstocks are truly a lifesaver for long days. The
+                    comfort and durability are outstanding. I'm so happy I chose
+                    to buy from you.
+                  </p>
+                  <img
+                    class="testi-image"
+                    src="images/HC4.jpg"
+                    alt="lady-profile"
+                  />
+                  <em>Beverly, Illinois</em>
+                </div>
+                <div className="carousel-item">
+                  <p class="testimonial-text">
+                    Thank you, Harunzy Collection, for my new favorite sandals!
+                    These Birkenstocks epitomize comfort and style. I wear them
+                    everywhere and couldn't be more pleased with my purchase.
+                    Delighted!
+                  </p>
+                  <img
+                    class="testi-image"
+                    src="images/HC4.jpg"
+                    alt="lady-profile"
+                  />
+                  <em>Beverly, Illinois</em>
+                </div>
+                <div className="carousel-item">
+                  <p class="testimonial-text">
+                    Harunzy Collection deserves all the praise! From browsing to
+                    delivery, the experience was fantastic. My Birkenstocks are
+                    incredibly comfortable, and I'm so grateful for the quality
+                    and genuine happiness they bring to my feet. Highly
+                    recommend!
                   </p>
                   <img
                     class="testi-image"
