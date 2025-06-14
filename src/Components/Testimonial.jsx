d57677b4fa5d26007bada6b4d5b4e5efe8364e92
Sir Harunzy <Harunzy55@gmail.com>
2025-06-14 23:53:23 +0000

    add

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -13,49 +13,49 @@ export default function Testimonial() {
             >
               <div className="carousel-inner">
                 <div className="carousel-item active ">
-                  <p class="testimonial-text">
+                  <p className="testimonial-text">
                     Harunzy Collection made my day! The Birkenstocks I ordered
                     fit perfectly and feel like walking on clouds. I'm so happy
                     with the quality and the excellent customer service. Thank
                     you!
                   </p>
                   <img
-                    class="testi-image"
+                    className="testi-image"
                     src="images/HC8.jpg"
                     alt="lady-profile"
                   />
                   <em>Abdulai, Savelugu</em>
                 </div>
                 <div className="carousel-item">
-                  <p class="testimonial-text">
+                  <p className="testimonial-text">
                     Five stars for Harunzy Collection! These Birkenstocks are a
                     game-changer. My daily walks are now a joy thanks to the
                     incredible support and comfort. So glad I found your
                     business!
                   </p>
                   <img
-                    class="testi-image"
+                    className="testi-image"
                     src="images/HC8.jpg"
                     alt="lady-profile"
                   />
                   <em>Abubakari, Shishagu</em>
                 </div>
                 <div className="carousel-item">
-                  <p class="testimonial-text">
+                  <p className="testimonial-text">
                     I can't express how much I love my Birkenstocks from Harunzy
                     Collection. They are incredibly comfortable and stylish
                     exactly what I needed. So thankful for your amazing
                     selection and service!
                   </p>
                   <img
-                    class="testi-image"
+                    className="testi-image"
                     src="images/HC10.jpg"
                     alt="lady-profile"
                   />
                   <em>Mr. Lari, Bolga</em>
                 </div>
                 <div className="carousel-item">
-                  <p class="testimonial-text">
+                  <p className="testimonial-text">
                     A huge thank you to Harunzy Collection! I've been wanting a
                     pair of Birkenstocks for ages, and these are everything I
                     hoped for and more. My feet have never been happier.
@@ -69,7 +69,7 @@ export default function Testimonial() {
                   <em>Mr. Zak, Diare</em>
                 </div>
                 <div className="carousel-item">
-                  <p class="testimonial-text">
+                  <p className="testimonial-text">
                     Harunzy Collection, you've completely won me over! My new
                     Birkenstocks are perfect. The comfort is unmatched, and I'm
                     so grateful for how quickly they arrived. Truly happy with
@@ -83,63 +83,63 @@ export default function Testimonial() {
                   <em>Mr. Rashid, Diare</em>
                 </div>
                 <div className="carousel-item">
-                  <p class="testimonial-text">
+                  <p className="testimonial-text">
                     Honestly, I'm beyond impressed with Harunzy Collection. My
                     new sandals are not only fashionable but also provide the
                     best comfort I've ever experienced. So grateful for such a
                     wonderful product!
                   </p>
                   <img
-                    class="testi-image"
+                    className="testi-image"
                     src="images/HC4.jpg"
                     alt="lady-profile"
                   />
                   <em>Mr. Mukim, Waa</em>
                 </div>
                 <div className="carousel-item">
-                  <p class="testimonial-text">
+                  <p className="testimonial-text">
                     What a find Harunzy Collection is! My Birkenstocks arrived
                     beautifully packaged, and they are even better in person.
                     I'm incredibly grateful for the high-quality footwear and
                     the overall pleasant experience.
                   </p>
                   <img
-                    class="testi-image"
+                    className="testi-image"
                     src="images/HC4.jpg"
                     alt="lady-profile"
                   />
                   <em>Ibrahim, Tamale</em>
                 </div>
                 <div className="carousel-item">
-                  <p class="testimonial-text">
+                  <p className="testimonial-text">
                     My feet are singing praises to Harunzy Collection! These
                     Birkenstocks are truly a lifesaver for long days. The
                     comfort and durability are outstanding. I'm so happy I chose
                     to buy from you.
                   </p>
                   <img
-                    class="testi-image"
+                    className="testi-image"
                     src="images/HC4.jpg"
                     alt="lady-profile"
                   />
                   <em>Joe, Tamale</em>
                 </div>
                 <div className="carousel-item">
-                  <p class="testimonial-text">
+                  <p className="testimonial-text">
                     Thank you, Harunzy Collection, for my new favorite sandals!
                     These Birkenstocks epitomize comfort and style. I wear them
                     everywhere and couldn't be more pleased with my purchase.
                     Delighted!
                   </p>
                   <img
-                    class="testi-image"
+                    className="testi-image"
                     src="images/HC4.jpg"
                     alt="lady-profile"
                   />
                   <em>Dauda, Sunyani</em>
                 </div>
                 <div className="carousel-item">
-                  <p class="testimonial-text">
+                  <p className="testimonial-text">
                     Harunzy Collection deserves all the praise! From browsing to
                     delivery, the experience was fantastic. My Birkenstocks are
                     incredibly comfortable, and I'm so grateful for the quality
@@ -147,7 +147,7 @@ export default function Testimonial() {
                     recommend!
                   </p>
                   <img
-                    class="testi-image"
+                    className="testi-image"
                     src="images/HC4.jpg"
                     alt="lady-profile"
                   />
