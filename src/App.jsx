45b1379b0e091b30903c1064dc5bc935f2b839ad
Sir Harunzy <Harunzy55@gmail.com>
2025-06-16 16:35:55 +0000

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
 // import { useState } from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
-import Footer from './Components/Footer';
 import Products from './Components/Products';
 import './App.css';
-import Testimonial from './Components/Testimonial';
-import WhatsApp from './Components/WhatsApp';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Header />
-        <WhatsApp />
-        <Products />
-        <Testimonial />
-        <Footer />
-        <Routes>
-          {/* <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} /> */}
-          {/* <Route path="/contact" element={<Contact />} /> */}
-        </Routes>
-      </Router>
-    </>
+    <BrowserRouter>
+      <>
+        <header>
+          <Header />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Products />} />
+          </Routes>
+        </main>
+      </>
+    </BrowserRouter>
   );
 }
 
