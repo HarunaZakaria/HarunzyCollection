2d9df2d23e6b658f53a6201bef8075ddecb3717b
Sir Harunzy <Harunzy55@gmail.com>
2025-07-05 17:06:11 +0000

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 // import { useState } from 'react';
+import data from './data';
+
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import Products from './Components/Products';
@@ -12,11 +14,30 @@ function App() {
         <header>
           <Header />
         </header>
+
+        <Products />
+        <Footer />
         <main>
-          <Routes>
-            <Route path="/" element={<Products />} />
-            <Route path="/" element={<Footer />} />
-          </Routes>
+          <h1>New Products</h1>
+          <div className="products">
+            {data.products.map((product) => (
+              <div className="product" key={product.countInStock}>
+                <a href={`/product${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+                <div className="product-info">
+                  <a href={`/product${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+
+                  <p>
+                    <strong>{product.price} </strong>
+                  </p>
+                  <button>Add to cart</button>
+                </div>
+              </div>
+            ))}
+          </div>
         </main>
       </>
     </BrowserRouter>
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,4 +1,3 @@
-import data from '../data';
 import Testimonial from './Testimonial';
 
 export default function Products() {
@@ -46,28 +45,6 @@ export default function Products() {
       {/* Categories section end */}
 
       {/* Feature products section end */}
-      <main>
-        <h1>New Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.countInStock}>
-              <a href={`/product${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-
-                <p>
-                  <strong>{product.price} </strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
 
       <div className="row feature-sandals">
         <h2>Feature Sandals</h2>
